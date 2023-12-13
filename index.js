7218507e8a31a7e86aca3e6cda64dab3da0b5a9b
dkiriinya <dkiriinya06sc@gmail.com>
2023-12-13 11:07:29 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(){
+    return 2000/100
+}
+console.log(divide())
+
+const square = (num1) => num1 * num1
+console.log(square(8))
+
+const add = (num1,num2) => num1 + num2
+console.log(add(78,98))
